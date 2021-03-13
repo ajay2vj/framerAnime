37160c6862ea162965ddf01d50081f40d8d6c8b1
Ajay M <ajay@scodesoft.com>
2021-03-13 13:42:32 +0530

    in home page animation hover reload and more

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import { React } from "react";
 import { Nav, Navbar } from "react-bootstrap";
+import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import "../styling/style.css";
 
 function NavBar() {
     return (
-        <>
+        <motion.div>
 					<Navbar collapseOnSelect expand="lg" bg="dark">
-							<Navbar.Brand href="#home">PizzaHut</Navbar.Brand>
+							<Navbar.Brand href="#home"><motion.div whileHover={{ scale: 1.2, rotate: 360 }}
+							whileTap={{
+								scale: 0.8,
+								rotate: -90,
+								borderRadius: "100%"
+							}}>PizzaHut</motion.div></Navbar.Brand>
 							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 							<Navbar.Collapse id="responsive-navbar-nav">
 									<Nav className="ml-auto">
@@ -17,7 +23,7 @@ function NavBar() {
 									</Nav>
 							</Navbar.Collapse>
 					</Navbar>
-      </>
+      </motion.div>
     )
 }
 export default NavBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,43 @@
 import {React} from "react";
-
+import { Container, Row, Col } from "react-bootstrap";
+import { motion } from "framer-motion";
+import { Button } from 'react-bootstrap';
 function Home(){
 
     return(
 			<>
 				<section id="home-section"></section>
+				
+				<section className="banner-content">
+          <Container>
+            <Row>
+						<Col md={4}></Col>
+              <Col md={4}>
+								
+									<div className="sub-content-banner">
+									<motion.div animate={{ rotate: 360 }} transition={{ duration: 2 }}>
+										<motion.h2 animate={{color:'#9c9b9b'}}>PizzaHut</motion.h2>
+									</motion.div>
+										<motion.div initial={{opacity: 0}}
+										 animate={{opacity: 1}}
+										 transition={{delay:0.5, duration:2}}
+										>
+											<p style={{color:'#fff'}}>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+										</motion.div>
+										<motion.div whileHover={{ scale: 1.2, rotate: 1,
+										boxShadow: '0px 0px 8px rgb(255,255,255)'}}
+											whileTap={{
+												scale: 0.2,
+												rotate: -90,
+											}}
+											><Button variant="primary" className="btn-anime">welcome</Button></motion.div>
+									</div>
+								
+              </Col>
+            </Row>
+          </Container>
+        </section>
+				
 			</>
     )
 }
